39b51ec6a07e71b6efadde146d8672c4de1f6970
Felix Neubauer <felix@neuby.de>
2023-06-23 22:15:10 +0200

    Add code to clone repos

---
+++ dataset_generator/dataset_generator/clone_dependent_repos.py
@@ -0,0 +1,18 @@
+from git import Repo
+import os
+from pathlib import Path
+
+import csv
+
+if __name__ == '__main__':
+    dir_name = os.path.dirname(os.path.realpath(__file__))
+    root_path = Path(dir_name).parent
+    dependents_csv_path = root_path.joinpath("collected_dependents").joinpath("dependents_apollo-client.csv")
+    result_repositories_path = root_path.joinpath("collected_repos")
+
+    with open(dependents_csv_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            repo_name = row['name']
+            print("Start cloning Repo " + repo_name + ".")
+            repo = Repo.clone_from("https://github.com/" + repo_name, result_repositories_path.joinpath(repo_name))
