ec9891473402a0c66b8dcaad1bfa99a61834535e
Felix Neubauer <felix@neuby.de>
2023-06-24 00:20:47 +0200

    begin finetune code

--- finetuning/finetune.py
+++ finetuning/finetune.py
@@ -1,7 +1,9 @@
 import os
 from pathlib import Path
+import nltk
+import string
+from transformers import AutoTokenizer
 
-import transformers
 from datasets import load_dataset, load_metric
 
 if __name__ == '__main__':
@@ -11,4 +13,8 @@ if __name__ == '__main__':
     dataset_path = root_path.joinpath("dataset_generator").joinpath("dataset.json")
 
     datasets = load_dataset("json", data_files=str(dataset_path))
+
+    nltk.download('punkt')
+    model_checkpoint = "t5-base"
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
     print("dw")
