2ce502047234770512eb9909b7f26a45ded41745
Felix Neubauer <felix@neuby.de>
2023-06-25 00:31:07 +0200

    Make cloning code endless and repeat after exception

--- dataset_generator/dataset_generator/clone_dependent_repos.py
+++ dataset_generator/dataset_generator/clone_dependent_repos.py
@@ -10,13 +10,23 @@ if __name__ == '__main__':
     dependents_csv_path = root_path.joinpath("collected_dependents").joinpath("dependents_apollo-client.csv")
     result_repositories_path = root_path.joinpath("collected_repos")
 
-    with open(dependents_csv_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            repo_name: str = row['name']
-            result_folder = result_repositories_path.joinpath(repo_name.replace("/", "_"))
-            if result_folder.exists():
-                print("Folder " + str(result_folder) + " already exists. Not cloning again.")
-            else:
-                print("Start cloning Repo " + repo_name + ".")
-                repo = Repo.clone_from("https://github.com/" + repo_name, result_folder)
+    print("Begin endless cloning process.")
+
+    while True:
+        with open(dependents_csv_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            try:
+                print("Attempt cloning of Github Repos.")
+
+                for row in reader:
+                    repo_name: str = row['name']
+                    result_folder = result_repositories_path.joinpath(repo_name.replace("/", "_"))
+                    if result_folder.exists():
+                        print("Folder " + str(result_folder) + " already exists. Not cloning again.")
+                    else:
+                        print("Start cloning Repo " + repo_name + ".")
+                        repo = Repo.clone_from("https://github.com/" + repo_name, result_folder)
+
+            except Exception as e:
+                print("Exception occurred: '" + str(e) + "'.")
