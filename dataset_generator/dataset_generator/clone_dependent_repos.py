c1bee61f921ccb9022a1382994ad41a8708274f1
Felix Neubauer <felix@neuby.de>
2023-06-25 13:20:08 +0200

    Allow parallel cloning

--- dataset_generator/dataset_generator/clone_dependent_repos.py
+++ dataset_generator/dataset_generator/clone_dependent_repos.py
@@ -22,11 +22,16 @@ if __name__ == '__main__':
                 for row in reader:
                     repo_name: str = row['name']
                     result_folder = result_repositories_path.joinpath(repo_name.replace("/", "_"))
+                    progress_file = result_repositories_path.joinpath(repo_name + ".progress")
                     if result_folder.exists():
                         print("Folder " + str(result_folder) + " already exists. Not cloning again.")
+                    elif progress_file.exists():
+                        print("Seems like another process is already cloning" + str(result_folder) + ".")
                     else:
                         print("Start cloning Repo " + repo_name + ".")
+                        open(str(progress_file), 'a').close()
                         repo = Repo.clone_from("https://github.com/" + repo_name, result_folder)
+                        os.remove(str(progress_file))
 
             except Exception as e:
                 print("Exception occurred: '" + str(e) + "'.")
