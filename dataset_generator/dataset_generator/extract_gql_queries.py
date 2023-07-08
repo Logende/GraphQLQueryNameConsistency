357f70ab3634d0e1a64ff400ca97df0addcd5d61
Felix Neubauer <felix@neuby.de>
2023-07-08 12:06:42 +0200

    Remove debugging

--- dataset_generator/dataset_generator/extract_gql_queries.py
+++ dataset_generator/dataset_generator/extract_gql_queries.py
@@ -57,7 +57,6 @@ def extract_gql_sections_from_text(data: str) -> List[str]:
         for _, match in enumerate(matches, start=1):
             all_results.append(match.group())
             index += 1
-            print("match index " + str(index))
     return all_results
 
 
